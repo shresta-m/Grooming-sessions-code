49d07a683e13076349ffc28d18e7cb9322f134c6
Shresta M <71119133+shresta-m@users.noreply.github.com>
2023-04-18 14:54:12 +0530

    Add files via upload

---
+++ Week-2/GenericSort.java
@@ -0,0 +1,34 @@
+package week2;
+
+import java.util.Arrays;
+
+public class GenericSort {
+    static <T extends Comparable<T>> void sort(T[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = 0; j < n-i-1; j++){
+                 if (arr[j].compareTo(arr[j+1])> 0) {
+                      T temp = arr[j];
+                      arr[j] = arr[j+1];
+                      arr[j+1] = temp;
+                 }
+             }
+        }
+    }
+
+    public static void main(String args[]){
+        String[] a1 = {"T","Z","A"};
+        sort(a1);
+        System.out.println(Arrays.toString(a1));
+        Integer[] a2 = {12,100,4,55,19034,34256};
+        sort(a2);
+        System.out.println(Arrays.toString(a2));
+        Character[] a3 = {'z','a','s','m','t'};
+        sort(a3);
+        System.out.println(Arrays.toString(a3));
+
+
+
+    }
+    
+}
