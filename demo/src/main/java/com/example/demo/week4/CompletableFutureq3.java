be11d6908b239efa63134dc16f70ca35a4c74a62
Shresta M <71119133+shresta-m@users.noreply.github.com>
2023-05-02 18:51:08 +0530

    Update CompletableFutureq3.java

--- demo/src/main/java/com/example/demo/week4/CompletableFutureq3.java
+++ demo/src/main/java/com/example/demo/week4/CompletableFutureq3.java
@@ -1,48 +1,36 @@
 package com.example.demo.week4;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureq3 {
-
-    public static Integer sumOne() throws InterruptedException, ExecutionException{
-        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(()->{
-            int sum =0;
-            for(int i=1;i<=10;i++){
-                sum+=i;
-            }
-            return sum;
-        });
-        return result.get();
-    }
-
-    public static Integer sumTwo() throws InterruptedException, ExecutionException{
-        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(()->{
-            int sum =0;
-            for(int i=11;i<=20;i++){
-                sum+=i;
-            }
-            return sum;
-        });
-        return result.get();
-    }
-
-    public static Integer sumThree() throws InterruptedException, ExecutionException{
-        CompletableFuture<Integer> result = CompletableFuture.supplyAsync(()->{
-            int sum =0;
-            for(int i=21;i<=30;i++){
-                sum+=i;
-            }
-            return sum;
-        });
-        return result.get();
-    }
 
     public static void main(String args[]){
 
         try{
-            int result = sumOne()+sumTwo()+sumThree();
-            System.out.println("The sum from all 3 completable future results is "+result);
+            CompletableFuture<Integer> result1 = CompletableFuture.supplyAsync(()->{
+                int sum =0;
+                for(int i=1;i<=10;i++){
+                    sum+=i;
+                }
+                return sum;
+            });
+            CompletableFuture<Integer> result2 = CompletableFuture.supplyAsync(()->{
+                int sum =0;
+                for(int i=11;i<=20;i++){
+                    sum+=i;
+                }
+                return sum;
+            });
+            CompletableFuture<Integer> result3 = CompletableFuture.supplyAsync(()->{
+                int sum =0;
+                for(int i=21;i<=30;i++){
+                    sum+=i;
+                }
+                return sum;
+            });
+            CompletableFuture<Integer> finalResult = result1.thenCombine(result2,(a,b)-> a+b)
+                    .thenCombine(result3,(ab,c)-> ab+c);
+            System.out.println("The sum from all 3 completable future results is "+finalResult.join());
 
         }catch(Exception e){
             System.out.println(e.getMessage());
