c8a5f6a4f435aae20d79ccca41dfac94ca8232fa
Shresta M <71119133+shresta-m@users.noreply.github.com>
2023-05-02 16:26:12 +0530

    Update CountInstance.java

--- demo/src/main/java/com/example/demo/week4/CountInstance.java
+++ demo/src/main/java/com/example/demo/week4/CountInstance.java
@@ -16,33 +16,18 @@ public class CountInstance extends RecursiveTask<Integer> {
 
     @Override
     protected Integer compute() {
-        int arrLength = end - start + 1;
-        if (arrLength == threshold)
+       int arrLength = end - start + 1;
+        if (arrLength <= threshold) {
             return countInstance();
+        }
         else {
-            CountInstance subtask_1 = new CountInstance(arr, 0, threshold-1);
-            CountInstance subtask_2 = new CountInstance(arr, threshold, threshold*2-1);
-            CountInstance subtask_3 = new CountInstance(arr, threshold*2, threshold*3-1);
-            CountInstance subtask_4 = new CountInstance(arr, threshold*3,threshold*4-1);
-            CountInstance subtask_5 = new CountInstance(arr, threshold*4,threshold*5-1);
-            CountInstance subtask_6 = new CountInstance(arr, threshold*5, threshold*6-1);
-            CountInstance subtask_7 = new CountInstance(arr, threshold*6, threshold*7-1);
-            CountInstance subtask_8 = new CountInstance(arr, threshold*7, threshold*8-1);
-            CountInstance subtask_9 = new CountInstance(arr, threshold*8,threshold*9-1);
-            CountInstance subtask_10 = new CountInstance(arr, threshold*9,threshold*10-1);
-             subtask_1.fork();
-             subtask_2.fork();
-             subtask_3.fork();
-             subtask_4.fork();
-             subtask_5.fork();
-             subtask_6.fork();
-             subtask_7.fork();
-             subtask_8.fork();
-             subtask_9.fork();
-             subtask_10.fork();
-             int count = subtask_1.join() + subtask_2.join() + subtask_3.join() + subtask_4.join() + subtask_5.join()
-                     + subtask_6.join() + subtask_7.join() + subtask_8.join() + subtask_9.join()+ subtask_10.join();
-             return count;
+            int mid = (start + end) / 2;
+            CountInstance subtask_1 = new CountInstance(arr, start, mid);
+            CountInstance subtask_2 = new CountInstance(arr, mid + 1, end);
+            subtask_1.fork();
+            subtask_2.fork();
+
+            return subtask_1.join() + subtask_2.join();
         }
 
     }
